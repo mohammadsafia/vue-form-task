f76f0435147a3ecabf74220cf332b1d5d4830334
Mohammad Safia <msafia@terkwaz.com>
2021-07-06 16:30:57 +0300

    Create Custom input as reusable component, add input styles

---
+++ src/components/shared/BaseInput.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="form-group">
+    <label :aria-invalid="invalid" :for="name">{{ label }}</label>
+    <input :aria-invalid="invalid" :type="type" class="form-control" :id="name" :placeholder="placeholder" autocomplete="off" :value="value" v-on:change="onChange">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseInput',
+  props: {
+    type: {
+      type: String,
+      default: 'text',
+    },
+    name: {
+      type: String,
+    },
+    placeholder: {
+      type: String,
+    },
+    label: {
+      type: String,
+    },
+    value: {
+      type: String,
+    },
+    invalid: {
+      type:Boolean,
+      default: false
+    }
+  },
+  methods: {
+    onChange(event) {
+      this.$emit('onChangeHandler', event.target.value);
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/shared/FormCard.vue
@@ -0,0 +1,12 @@
+<template>
+  <form class="base-form">
+    <slot></slot>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'BaseForm',
+};
+</script>
+
